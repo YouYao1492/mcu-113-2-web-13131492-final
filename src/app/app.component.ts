e3887fd389527df1af5af884e0590a0e8699f20c
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-30 01:44:04 +0800

    feat(product): 依產品清單顯示實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { ProductCardListComponent } from './product-card-list/product-card-list.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  products = [
+    new Product({
+      id: 'A',
+      name: '產品',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+
+    new Product({
+      id: 'B',
+      name: '產品',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+
+    new Product({
+      id: 'C',
+      name: '產品',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+
+    new Product({
+      id: 'D',
+      name: '產品',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+  ];
+}
--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../models/product';
 
@@ -9,16 +9,8 @@ import { Product } from '../models/product';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  product = new Product({
-    id: 'A',
-    name: '產品',
-    authors: '作者甲、作者乙、作者丙',
-    company: '博碩文化',
-
-    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
-
-    price: 1580,
-  });
+  @Input()
+  products!: Product[];
 
   detail(): void {
     console.log('detail');
