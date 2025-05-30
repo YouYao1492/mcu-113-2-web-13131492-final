fa1d744c09e0c45c789ed44b2a03a28b9bc607c1
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-30 16:34:21 +0800

    refactor(product): 調整使用 signal 參數實作

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,5 @@
 import { PaginationComponent } from './../pagination/pagination.component';
-import { Component, Input } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
 import { Product } from '../models/product';
 
@@ -10,15 +10,7 @@ import { Product } from '../models/product';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  @Input()
-  products!: Product[];
+  readonly products = input<Product[]>([]);
 
   pageIndex = 1;
-
-  detail(): void {
-    console.log('detail');
-  }
-  add(): void {
-    console.log('add');
-  }
 }
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, HostBinding, Input, numberAttribute } from '@angular/core';
+import { Component, HostBinding, input, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
@@ -8,23 +8,17 @@ import { Component, HostBinding, Input, numberAttribute } from '@angular/core';
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
-  @Input({ required: true })
-  id!: string;
+  readonly id = input.required<string>();
 
-  @Input()
-  productName!: string;
+  readonly productName = input<string>();
 
-  @Input()
-  authors!: string[];
+  readonly authors = input<string[]>();
 
-  @Input()
-  company!: string;
+  readonly company = input<string>();
 
-  @Input()
-  photoUrl!: string;
+  readonly photoUrl = input<string>();
 
-  @Input({ transform: numberAttribute })
-  price!: number;
+  readonly price = input<number, number | string>(0, { transform: numberAttribute });
 
   @HostBinding('class')
   class = 'app-product-card';
