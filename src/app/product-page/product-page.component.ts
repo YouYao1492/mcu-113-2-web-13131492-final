47ac7308f954f9f151b195bac1853dea8cc106a5
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-30 22:01:40 +0800

    feat(product): 實作產品資料分頁

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
+import { PaginationComponent } from './../pagination/pagination.component';
 import { ProductService } from './../services/product.service';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardListComponent],
+  imports: [PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
@@ -15,10 +16,16 @@ export class ProductPageComponent implements OnInit {
 
   private productService = inject(ProductService);
 
+  pageIndex = 1;
+
+  pageSize = 5;
+
+  totalCount = 0;
+
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.products = this.productService.getList();
+    this.getProducts();
   }
 
   onView(product: Product): void {
@@ -27,5 +34,16 @@ export class ProductPageComponent implements OnInit {
 
   onAdd(product: Product): void {
     console.log(product);
+  }
+
+  onPageIndexChange(pageIndex: number): void {
+    this.pageIndex = pageIndex;
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    const { data, count } = this.productService.getList(undefined, this.pageIndex, this.pageSize);
+    this.products = data;
+    this.totalCount = count;
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product';
+import { enableDebugTools } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root',
@@ -41,9 +42,52 @@ export class ProductService {
       photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
       price: 1580,
     }),
+    new Product({
+      id: 'E',
+      name: '產品',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 'F',
+      name: '產品',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 'G',
+      name: '產品',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 'H',
+      name: '產品',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
+    new Product({
+      id: 'I',
+      name: '產品',
+      authors: ['作者甲', '作者乙', '作者丙'],
+      company: '博碩文化',
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+      price: 1580,
+    }),
   ];
 
-  getList(): Product[] {
-    return this._data;
+  getList(name: string | undefined, index: number, size: number): { data: Product[]; count: number } {
+    const startIndex = (index - 1) * size;
+    const endIndex = startIndex + size;
+    const data = name ? this._data.filter((item) => item.name === name) : [...this._data];
+    return { data: data.slice(startIndex, endIndex), count: this._data.length };
   }
 }
