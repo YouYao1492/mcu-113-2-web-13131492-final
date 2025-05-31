0699e400c7b43864f48d4e67f5878a288684a01e
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-31 16:39:15 +0800

    feat(resolver): 建立產品資料預載資料

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
 import { ProductDetailPageComponent } from './product-detail-page/product-detail-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { productResolver } from './resolver/product.resolver';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'products' },
   { path: 'products', component: ProductPageComponent },
-  { path: 'products/:id', component: ProductDetailPageComponent },
+  { path: 'products/:id', component: ProductDetailPageComponent, resolve: { product: productResolver } },
   { path: 'cart', component: CartComponent },
 ];
--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,8 +1,8 @@
+import { ProductService } from './../services/product.service';
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Product } from '../models/product';
-import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
@@ -10,18 +10,12 @@ import { ProductService } from '../services/product.service';
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent implements OnInit {
-  id = input.required<string>();
-
-  product!: Product;
+export class ProductDetailPageComponent {
+  readonly product = input.required<Product>();
 
   readonly router = inject(Router);
 
-  private productService = inject(ProductService);
-
-  ngOnInit(): void {
-    this.productService.getById(this.id()).subscribe((product) => (this.product = product));
-  }
+  private ProductService = inject(ProductService);
 
   onBack(): void {
     this.router.navigate(['products']);
