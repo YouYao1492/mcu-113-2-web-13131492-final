6eca9d9954cd767788d5000a53e17c888ed45450
陳宥耀 <13131492@me.mcu.edu.tw>
2025-05-31 16:08:57 +0800

    refactor(product): 變更使用 toSignal 轉換 Observable

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
+import { BehaviorSubject, combineLatest, startWith, Subject, switchMap } from 'rxjs';
 import { Product } from '../models/product';
 import { ProductCardListComponent } from '../product-card-list/product-card-list.component';
 import { PaginationComponent } from './../pagination/pagination.component';
 import { ProductService } from './../services/product.service';
-import { BehaviorSubject, combineLatest, startWith, Subject, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-product-page',
@@ -12,7 +13,7 @@ import { BehaviorSubject, combineLatest, startWith, Subject, switchMap } from 'r
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
-export class ProductPageComponent implements OnInit {
+export class ProductPageComponent {
   private router = inject(Router);
 
   private productService = inject(ProductService);
@@ -29,18 +30,21 @@ export class ProductPageComponent implements OnInit {
 
   pageSize = 5;
 
-  totalCount = 0;
+  private readonly data$ = combineLatest([this.pageIndex$, this.refresh$.pipe(startWith(undefined))]).pipe(
+    switchMap(() => this.productService.getList(undefined, this.pageIndex, this.pageSize))
+  );
 
-  products: Product[] = [];
+  private readonly data = toSignal(this.data$, { initialValue: { data: [], count: 0 } });
 
-  ngOnInit(): void {
-    combineLatest([this.pageIndex$, this.refresh$.pipe(startWith(undefined))])
-      .pipe(switchMap(() => this.productService.getList(undefined, this.pageIndex, this.pageSize)))
-      .subscribe(({ data, count }) => {
-        this.products = data;
-        this.totalCount = count;
-      });
-  }
+  readonly totalCount = computed(() => {
+    const { count } = this.data();
+    return count;
+  });
+
+  readonly products = computed(() => {
+    const { data } = this.data();
+    return data;
+  });
 
   onView(product: Product): void {
     this.router.navigate(['products', product.id]);
@@ -49,4 +53,7 @@ export class ProductPageComponent implements OnInit {
   onAdd(product: Product): void {
     console.log(product);
   }
+}
+function compute(arg0: () => any) {
+  throw new Error('Function not implemented.');
 }
