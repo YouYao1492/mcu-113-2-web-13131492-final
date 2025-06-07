8198df5320645cb36540dd0ebdca02fcd6d283d4
陳宥耀 <13131492@me.mcu.edu.tw>
2025-06-07 12:16:04 +0800

    feat(cart): 基本資料表單姓名、地址、電話欄位設定

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,11 +1,17 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
-
+  form = new FormGroup({
+    name: new FormControl<string | null>(null),
+    address: new FormControl<string | null>(null),
+    phone: new FormControl<string | null>(null),
+  });
 }
